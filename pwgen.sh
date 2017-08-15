75ba47c9772f1eae77e1ae0212c9c4f2eb150e91
m00ny <TSCxxXMONSTAXxx@gmail.com>
2017-08-16 01:57:21 +0200

    redesign
    Redesigned almost the whole script.
    
    - removed old parameter system
    - removed old help screen
    + Added new parameter check system (dynamic) - at least part of
    + Added new help screen with examples and parameter list
    + Added Documentation
    + Added Functionality to generate a single password by just running the script without parameters for faster usage

--- pwgen.sh
+++ pwgen.sh
@@ -1,25 +1,78 @@
 #!/bin/sh
-if [ "$1" == "-h" ]
-then 
-	echo "usage: $0 password-length amount "
-	echo "-o describes the output file but is optional. IT MUST BE AT THE END!"
+
+# Default PARAMETERS
+amount=1
+length=8
+filename=pwlist.txt
+
+# Print the help screen
+printhelp()
+{
+	echo -e "$0 will generate 1 password with the length of 8"
+	echo -e "EXAMPLES:"
+	echo -e "\tgenerate 1 password with a length of 8:\n\t$0 "
+	echo -e "\tgenerate 10 passwords with a length of 8:\n\t$0 -l 8 -a 10 -o passlist.txt\n"
+	echo -e "PARAMETERS:"
+	echo -e '\t-h \t shows this screen\n'
+	echo -e '\t\t ### required: ###'
+	echo -e '\t-l \t defines the length of the generated passord(s) \t\t- default = 8'
+	echo -e '\t-a \t defines the amount of generated passwords \t\t\t- default = 1\n'
+	echo -e '\t\t ### optional: ###'
+	echo -e '\t-o \t defines the output file the passwords will be saved in \t- default = none'
+	echo -e '\t-c \t defines the charset to use //NOT IMPLEMENTED YET'
 	exit 0
-fi 
+}
 
-if [ $# -lt 2 ]
-then
-        echo "usage: $0 password-length amount "
-	echo "-o describes the output file but is optional. It must be at the end"
-	
-	else 
-		filename=pwlist.txt
-		if [ "$3" == "-o" ]
-	then 
-		filename="$4"
-	fi
+# write the generated passwords to console
+printpw()
+{
+	cat $filename
+}
 
-	length=$1
-	amount=$2
+# main password generation function
+passgen()
+{
+	for i in `seq 1 $2`; do strings /dev/urandom|grep -o '[[:alnum:]!#+*~<>|"§$%&/(),.;:_-]'|head -n $1|tr -d '\n';echo "";done > "$filename"
+	#TODO: check if you want to print
+	printpw
+	exit 0
+}
 
-for i in `seq 1 $length`; do strings /dev/urandom|grep -o '[[:alnum:]!#+*~<>|"§$%&/(),.;:_-]'|head -n $length|tr -d '\n';echo "";done > "$filename"
+# default generation without parameters
+if [ $# -eq 0 ]
+then passgen $length $amount
 fi
+# check if required parameters are set
+if [ $# -lt 4 ]
+then printhelp
+fi
+
+# check for first option & its parameter
+case $1 in
+	"-h") printhelp ;;
+	"-o") filename=$2 ;;
+	"-a") amount=$2 ;;
+	"-l") length=$2 ;;
+	"-c") echo "not implemented yet!" ;;
+	*) printhelp ;;
+esac
+
+# check second option & its parameter
+case $3 in
+	"-h") printhelp ;;
+	"-o") filename=$4 ;;
+	"-a") amount=$4 ;;
+	"-l") length=$4 ;;
+	"-c") echo "not implemented yet!" ;;
+	*) printhelp ;;
+esac
+
+# check third option & its parameter - if not given, generate the passwords
+case $5 in
+	"-h") printhelp ;;
+	"-o") filename=$6 ;;
+	"-a") amount=$6 ;;
+	"-l") length=$6 ;;
+	"-c") echo "not implemented yet!" ;;
+	*) passgen $length $amount ;;
+esac
